d20530f9748175c1e15c98343aefb0cc3420b568
Ashish Aphale <aphale.ashish@gmail.com>
2024-06-26 11:18:39 +0530

    pushed changes to staging branch

---
+++ ashish-azure-demo-vs/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ashish_azure_demo_vs.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
